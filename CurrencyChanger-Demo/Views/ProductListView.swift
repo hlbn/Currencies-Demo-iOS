654d08ddd05fd87732f0fcb4c5f5397c58093ccd
hlbn <adamhlubina@gmail.com>
2022-06-09 15:10:13 +0200

    Prisposobenie prostredia

---
+++ CurrencyChanger-Demo/Views/ProductListView.swift
@@ -0,0 +1,20 @@
+//
+//  ProductListView.swift
+//  CurrencyChanger-Demo
+//
+//  Created by Adam Hlubina on 09/06/2022.
+//
+
+import SwiftUI
+
+struct ProductListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProductListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProductListView()
+    }
+}

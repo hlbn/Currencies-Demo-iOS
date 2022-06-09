b4044915b0fa4770e22abd4c48b43ca917f26748
hlbn <adamhlubina@gmail.com>
2022-06-09 14:45:14 +0200

    Vytvorenie projektu

---
+++ CurrencyChanger-Demo/CurrencyChanger_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyChanger_DemoApp.swift
+//  CurrencyChanger-Demo
+//
+//  Created by Adam Hlubina on 09/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyChanger_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
